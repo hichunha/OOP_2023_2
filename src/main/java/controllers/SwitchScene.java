7ba878cd1200a99f6c0c21e17af7822c9d6ac96e
hichunha <105657745+hichunha@users.noreply.github.com>
2023-04-26 18:39:40 +0700

    Add files via upload

---
+++ src/main/java/controllers/SwitchScene.java
@@ -0,0 +1,34 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class SwitchScene {
+    public void SwitchScene(){
+
+    }
+    public void chagneMenu(ActionEvent e,String UI) throws IOException {
+        Parent root =  FXMLLoader.load(Objects.requireNonNull(getClass().getResource(UI))) ;
+        Stage stage = (Stage) ((MenuItem) e.getSource()).getParentPopup().getOwnerWindow();
+        stage.setScene(new Scene(root, 1200,640));
+        stage.centerOnScreen();
+        stage.show();
+    }
+    @FXML
+    public void changScene(ActionEvent e,String UI) throws IOException {
+        Parent root =  FXMLLoader.load(Objects.requireNonNull(getClass().getResource(UI))) ;
+        Stage stage= (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root, 1200,640));
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
